05592370de589319fd24e63847af347e0926cdb4
alridwanluthfi00 <luthfialridwan@gmail.com>
2021-01-11 20:44:54 +0700

    Tugas UAS

---
+++ View/input_nilai.py
@@ -0,0 +1,15 @@
+from data import data
+
+
+while True:
+    print("")
+    c = input("(T)ambah : ")
+    if c.data() == 't':
+        nama = input("Nama                :  ")
+        nim = input("Nim                 :  ")
+        tugas = int(input("Masukan Nilai Tugas :  "))
+        uts = int(input("Masukan Nilai UTS   :  "))
+        uas = int(input("Masukan Nilai UAS   :  "))
+        akhir = (0.30 * tugas) + (0.35 * uts) + (0.35 * uas)
+        data[nama] = nim, tugas, uts, uas, akhir
+        break
